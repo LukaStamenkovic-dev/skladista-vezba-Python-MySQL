2b98d00bf37bca1720d8dd3fd81b5dbb4f44c470
Luka Stamenkovic <luka_stamenkovic@hotmail.com>
2026-06-15 17:47:09 +0200

    Added order_items.py and order.py in models

---
+++ models/order.py
@@ -0,0 +1,12 @@
+class Order:
+    def __init__(self, status, expected_delivery_date, total_price, id = None, created_at = None):
+        self.status = status
+        self.expected_delivery_date = expected_delivery_date
+        self.total_price = total_price
+        self.created_at = created_at
+
+
+    # def __str__(self):
+        # return f'Product ID: {self.id} \n Name: {self.name} \n Available: {available} \n Price: {self.price}'
+    
+
---
+++ models/order_items.py
@@ -0,0 +1,11 @@
+class OrderItem:
+    def __init__(self, order_id, product_id, quantity, price, id = None):
+        self.order_id = order_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.price = price
+        self.id = id
+
+
+
+
